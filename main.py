8d1b2fa634d5d9726112fc8478881cb9844552b3
MaYOR <denysartemmoskvytyn@gmail.com>
2023-04-12 01:26:46 +0300

    1_lab Done

---
+++ main.py
@@ -0,0 +1,9 @@
+from colorama import Fore, Style
+
+print(f'{Fore.GREEN}\n\tMenu:'
+      f'\n\t\t1. Lab-1 ( Алгоритм Крускала )'
+      f'\n\t\t2. Lab-2 ( Задача листоноші )'
+      f'\n\t\t3. Lab-3 ( Задача комівояжера )'
+      f'\n\t\t4. Lab-4 ( Потокові алгоритми )'
+      f'\n\t\t5. Lab-5 ( Ізоморфізм графів )'
+      f'{Style.RESET_ALL}')
